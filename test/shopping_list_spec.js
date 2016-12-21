8ea07c5e4e10c39a4b12878fd82cc94c8e3cfce5
Tyler Nichols <tylernchls@gmail.com>
2016-12-20 16:40:16 -1000

    add fail ShoppingListItem shoud be a class

--- test/shopping_list_spec.js
+++ test/shopping_list_spec.js
@@ -1,3 +1,8 @@
 const expect =  chai.expect;
 chai.should();
 
+describe('ShoppingListItem', function () {
+  it('should be a class', function () {
+    ShoppingListItem.should.be.a('function');
+  });
+});
