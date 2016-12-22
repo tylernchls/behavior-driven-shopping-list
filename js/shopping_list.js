2083802eb71a46da89697cf94fa1de5dad5f82da
Tyler Nichols <tylernchls@gmail.com>
2016-12-21 15:37:28 -1000

    all tests have passed

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -1,23 +1,3 @@
-class ShoppingListItem{
-  constructor(name, description) {
-    this.name = name;
-    this.description = description;
-    this.is_done = false;
-  }
-
-  check() {
-    this.is_done = true;
-  }
-
-  uncheck() {
-    this.is_done;
-  }
-
-  render() {
-    return `<li class="completed_${this.is_done}"><span>${this.name}</span> <span>${this.description}</span></li>`;
-  }
-}
-
 
 class ShoppingList{
   constructor(){
---
+++ js/shopping_list_item.js
@@ -0,0 +1,19 @@
+class ShoppingListItem{
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.is_done = false;
+  }
+
+  check() {
+    this.is_done = true;
+  }
+
+  uncheck() {
+    this.is_done;
+  }
+
+  render() {
+    return `<li class="completed_${this.is_done}"><span>${this.name}</span> <span>${this.description}</span></li>`;
+  }
+}
