939d50f67ae7396de01c04611a9c6abf3e8f424c
Tyler Nichols <tylernchls@gmail.com>
2016-12-22 13:06:43 -1000

    fixed stylesheet

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -17,24 +17,7 @@ class ShoppingListItem{
   }
 
   render() {
-    // var btn = document.createElement('button');
-    // var x = document.createTextNode('x');
-    // btn.appendChild(x);
-
-    // var li = documenent.createElement('li');
-    // //add span
-    // // add another span
-    // // add content
-    // var deleteBtn = documenent.createElement('button');
-    // deleteBtn.innerHTML = '&times';
-    // deleteBtn.addEventListener('click', () => {
-    //   console.log('delete this ShoppingListItem');
-    // })
-    // li.appendChild(deleteBtn);
-
-    // // add deleteBtn to li
-
-
     return `<li class="completed_${this.is_done}" id="${this._idx}"><span>${this.name}</span> <span>${this.description}</span><input type="checkbox" onchange="changeCheckedStatus('${this._idx}', this)"> <button onclick="removeItemButtonClicked('${this._idx}')">x</button></li>`;
   }
 }
+
